61911bb2771508b4b0aaf1d078d5ef752e377bee
Finidi47 <finidisaid@gmail.com>
2025-07-15 11:15:54 +0300

    Style the MainContent

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent(){
     return(
-        <main>
-        <   p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{backgroundColor:"#b5d779", height:"100px", alignContent:"center"}}>
+        <p style={{color:"tomato"}}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
