151ca853d1f148543392e298da785b1094f29f5b
Finidi47 <finidisaid@gmail.com>
2025-07-19 09:35:26 +0300

    Initial Code with Prop Drilling

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ProfilePage from './ProfilePage';
+
+
+
+function App(){
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" }
+  return(
+    <ProfilePage userData={userData}/>
+  );
+}
+
+export default App
---
+++ alx-react-app-props/src/UserDetails.jsx
@@ -0,0 +1,12 @@
+
+
+function UserDetails({userData}){
+    return(
+        <div>
+            <p>Name: {userData.name}</p>
+            <p>Email: {userData.email}</p>
+        </div>
+    );
+}
+
+export default UserDetails;
