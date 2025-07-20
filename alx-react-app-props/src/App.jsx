f3540b9daeb93bac2f20efa51862d7bc102d3562
Finidi47 <finidisaid@gmail.com>
2025-07-20 23:32:29 +0300

    Checker fix

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,11 +1,12 @@
-import ProfilePage from './components/ProfilePage';
+
 import UserContext from './components/UserContext';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return <UserContext.Provider value={userData} >
-            <ProfilePage />
+            <UserProfile />
          </UserContext.Provider>;
 }
 
