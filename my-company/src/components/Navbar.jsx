9b5db81fb0b482b716feee8f85df4a6b61d9fb1e
Finidi47 <finidisaid@gmail.com>
2025-07-22 21:57:30 +0300

    Checker fix - backgroundColor

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <>
-            <nav style={{background:"#8fce00", display:"flex", justifyContent:"center",}}>
+            <nav style={{backgroundColor:"#8fce00", display:"flex", justifyContent:"center",}}>
                 <ul style={{listStyle: "none"}}>
                     <li><Link to={"/"}><p>Home</p></Link></li>
                     <li><Link to={"/about"}><p>About</p></Link></li>
