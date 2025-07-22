fb4653c0d85559418968fd6b7cb0c07ee82914da
Finidi47 <finidisaid@gmail.com>
2025-07-22 21:55:25 +0300

    Checks for the inline styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <>
-            <nav>
-                <ul>
+            <nav style={{background:"#8fce00", display:"flex", justifyContent:"center",}}>
+                <ul style={{listStyle: "none"}}>
                     <li><Link to={"/"}><p>Home</p></Link></li>
                     <li><Link to={"/about"}><p>About</p></Link></li>
                     <li><Link to={"/services"}><p>Services</p></Link></li>
