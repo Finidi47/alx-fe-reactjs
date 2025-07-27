e8e9146739bf67a14ffd011327c2e914360a1413
Finidi47 <finidisaid@gmail.com>
2025-07-27 11:47:36 +0300

    Enhancing the app

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,29 @@
+import { useParams } from 'react-router-dom';
+import useRecipeStore from '../store/recipeStore'; 
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
+
+const RecipeDetails = () => {
+  const { recipeId } = useParams();
+  const recipe = useRecipeStore(state =>
+    state.recipes.find(recipe => recipe.id === Number(recipeId))
+  );
+
+  if (!recipe) {
+    return <p>Recipe not found.</p>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+
+      <h2>Edit Recipe</h2>
+      <EditRecipeForm recipe={recipe} />
+
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
