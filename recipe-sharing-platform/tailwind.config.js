5abc4192fcb016a36530bc7176705521db810543
Finidi47 <finidisaid@gmail.com>
2025-08-17 03:00:03 +0300

    Setting up the app with Tailwind CSS

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+
+  darkMode: false,
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
